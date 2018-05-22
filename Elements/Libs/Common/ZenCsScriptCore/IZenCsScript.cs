f82c82b5b6f4d95fb0fa5fd8e343852dba22d565
Tomaž Vinko <tomaz@zenodys.com>
2018-05-22 09:40:48 +0200

    ZenCsScriptCore initial commit

---
+++ Elements/Libs/Common/ZenCsScriptCore/IZenCsScript.cs
@@ -0,0 +1,14 @@
+﻿using CommonInterfaces;
+using System.Collections;
+
+public interface IZenCsScript
+{
+    string RawHtml { get; }
+    object RunCustomCode(string functionName);
+#if NETCOREAPP2_0
+    void Init(Hashtable elements, IElement element);
+#else
+    void Init(Hashtable elements, IElement element, IZenCallable callable);
+#endif
+    int Order { get; }
+}
---
+++ Elements/Libs/Common/ZenCsScriptCore/ZenCsScriptData.cs
@@ -0,0 +1,20 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ZenCsScriptData
+{
+    public IZenCsScript ZenCsScript { get; set; }
+    public HtmlDocument ScriptDoc { get; set; }
+
+    public ZenCsScriptData(IZenCsScript ZenCsScript, HtmlDocument ScriptDoc)
+    {
+        this.ScriptDoc = ScriptDoc;
+        this.ZenCsScript = ZenCsScript;
+
+    }
+}
+
