76418655856e76131dafbb7c87fb8c9141771d4a
Tomaž Vinko <tomaz@zenodys.com>
2018-05-31 20:58:04 +0200

    ZenSqlServer: initial commit

---
+++ Elements/Common/ZenSqlServer/ZenSqlServer.cs
@@ -0,0 +1,263 @@
+﻿using ZenCommon;
+using HtmlAgilityPack;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+
+namespace ZenSqlServer
+{
+#if NETCOREAPP2_0
+    public class ZenSqlServer
+#else
+    public class ZenSqlServer : IZenAction //, IZenCallable
+#endif
+    {
+        #region Fields
+        #region _script
+        Dictionary<string, ZenCsScriptData> _scripts = new Dictionary<string, ZenCsScriptData>();
+        #endregion
+
+        /*#region _dynamicSpParams
+        Hashtable _dynamicSpParams = new Hashtable();
+        #endregion
+    */
+        #region _scriptData
+        ZenCsScriptData _scriptData;
+        #endregion
+
+        #region _syncCsScript
+        object _syncCsScript = new object();
+        #endregion
+        #endregion
+
+        /*
+        #region IZenCallable Implementations
+        #region Functions
+        public object Call(string actionID, Hashtable param)
+        {
+            switch (actionID)
+            {
+                case "ADD_SP_PARAM":
+                    foreach (DictionaryEntry pair in param)
+                        _dynamicSpParams[pair.Key] = pair.Value;
+                    break;
+
+                case "GET_SP_PARAM":
+                    foreach (DictionaryEntry pair in param)
+                        return _dynamicSpParams[pair.Key];
+                    break;
+
+                case "CLEAR_SP_PARAMS":
+                    _dynamicSpParams.Clear();
+                    break;
+            }
+            return null;
+        }
+        #endregion
+        #endregion
+    */
+
+#if NETCOREAPP2_0
+        #region _implementations
+        static Dictionary<string, ZenSqlServer> _implementations = new Dictionary<string, ZenSqlServer>();
+        #endregion
+
+        unsafe public static void InitManagedElements(string currentElementId, void** elements, int elementsCount, string projectRoot, string projectId, ZenNativeHelpers.GetElementProperty getElementPropertyCallback, ZenNativeHelpers.GetElementResultInfo getElementResultInfoCallback, ZenNativeHelpers.GetElementResult getElementResultCallback, ZenNativeHelpers.ExecuteElement executeElementCallback, ZenNativeHelpers.SetElementProperty setElementProperty)
+        {
+            if (!_implementations.ContainsKey(currentElementId))
+                _implementations.Add(currentElementId, new ZenSqlServer());
+
+            ZenNativeHelpers.InitManagedElements(currentElementId, elements, elementsCount, projectRoot, projectId, getElementPropertyCallback, getElementResultInfoCallback, getElementResultCallback, executeElementCallback, setElementProperty);
+        }
+        unsafe public static void ExecuteAction(string currentElementId, void** elements, int elementsCount, IntPtr result)
+        {
+            _implementations[currentElementId].ExecuteSql(ZenNativeHelpers.Elements, ZenNativeHelpers.Elements[currentElementId] as IElement, ZenNativeHelpers.ParentBoard);
+            ZenNativeHelpers.CopyManagedStringToUnmanagedMemory(string.Empty, result);
+        }
+#else
+        #region IZenAction Implementations
+        #region Properties
+        #region ID
+        public string ID { get; set; }
+        #endregion
+
+        #region ParentBoard
+        public IGadgeteerBoard ParentBoard { get; set; }
+        #endregion
+        #endregion
+
+        #region Functions
+        #region ExecuteAction
+        public void ExecuteAction(Hashtable elements, IElement element, IElement iAmStartedYou)
+        {
+            ExecuteSql(elements, element, ParentBoard);
+        }
+        #endregion
+        #endregion
+        #endregion
+#endif
+        #region Functions
+        #region ExecuteSql
+        void ExecuteSql(Hashtable elements, IElement element, IGadgeteerBoard ParentBoard)
+        {
+            SqlConnection conn = null;
+            try
+            {
+                string port = !string.IsNullOrEmpty(element.GetElementProperty("PORT")) ? "PORT=" + element.GetElementProperty("PORT").Trim() + ";" : string.Empty;
+                string connectionString = "Server=" + ZenCsScriptCore.Decode(element.GetElementProperty("SERVER")) + ";Database=" + element.GetElementProperty("DATABASE") + ";" + "User Id=" + element.GetElementProperty("UID") + ";" + "Password=" + element.GetElementProperty("PASSWORD") + ";";
+
+                using (conn = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand())
+                    {
+                        command.Connection = conn;
+                        switch (element.GetElementProperty("COMMAND_TYPE"))
+                        {
+                            case "1":
+                                command.CommandType = CommandType.Text;
+                                command.CommandText = GetSqlStatement(element.GetElementProperty("SQL_STATEMENT"), GetCacheFileName(element), elements, element, element.GetElementProperty("PRINT_CODE") == "1", ParentBoard);
+                                if (element.GetElementProperty("DO_DEBUG") == "1")
+                                    ParentBoard.PublishInfoPrint(element.ID, command.CommandText, "info");
+                                break;
+
+                            case "4":
+                                command.CommandType = CommandType.StoredProcedure;
+                                command.CommandText = element.GetElementProperty("STORED_PROCEDURE");
+                                if (!string.IsNullOrEmpty(element.GetElementProperty("SQLSERVER_PARAMETERS")))
+                                {
+                                    foreach (string s in element.GetElementProperty("SQLSERVER_PARAMETERS").Split('¨'))
+                                    {
+                                        if (!string.IsNullOrEmpty(s))
+                                            AddSqlServerParam(s.Split('°')[0], s.Split('°')[1], elements, element, command, ParentBoard);
+                                    }
+                                }
+
+                                /*if (_dynamicSpParams.Count > 0)
+                                {
+                                    foreach (DictionaryEntry kvp in _dynamicSpParams)
+                                    {
+                                        string preFeedbackScript = "Hashtable ht = new Hashtable();";
+                                        preFeedbackScript += "ht.Add(\"" + kvp.Key.ToString() + "\",null);";
+                                        string feedbackScript = "_callable.Call(\"GET_SP_PARAM\",ht)";
+                                        AddSqlServerParam(kvp.Key.ToString(), feedbackScript, preFeedbackScript, elements, element, command);
+                                    }
+                                }
+                                else if (!string.IsNullOrEmpty(element.GetElementProperty("SQLSERVER_PARAMETERS")))
+                                {
+                                    foreach (string s in element.GetElementProperty("SQLSERVER_PARAMETERS").Split('¨'))
+                                    {
+                                        if (!string.IsNullOrEmpty(s))
+                                            AddSqlServerParam(s.Split('°')[0], s.Split('°')[1], null, elements, element, command);
+                                    }
+                                }*/
+                                break;
+                            case "512":
+                                throw new Exception("ZenSqlServer : Table direct not supported yet!");
+                        }
+
+                        conn.Open();
+                        DataSet ds = new DataSet();
+                        SqlDataAdapter da = new SqlDataAdapter(command);
+                        da.Fill(ds);
+
+                        element.LastResultBoxed = ds;
+                        PrintDebug(element);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null && !string.IsNullOrEmpty(ex.InnerException.Message))
+                    throw new Exception(ex.InnerException.Message);
+                else if (!string.IsNullOrEmpty(ex.Message))
+                    throw new Exception(ex.Message);
+                else
+                    throw new Exception("Unknown Error in " + element.ID);
+            }
+            finally
+            {
+                if (conn != null && conn.State == ConnectionState.Open)
+                    conn.Close();
+
+                element.IsConditionMet = true;
+            }
+        }
+        #endregion
+
+        #region PrintDebug
+        void PrintDebug(IElement element)
+        {
+            if (element.GetElementProperty("DO_DEBUG") == "1")
+            {
+                Console.WriteLine();
+                Console.WriteLine(element.ID + " result details");
+
+                DataSet ds = (DataSet)element.LastResultBoxed;
+                for (int i = 0; i < ds.Tables.Count; i++)
+                {
+                    Console.WriteLine("Table : " + ds.Tables[i].TableName);
+                    Console.WriteLine("Columns: ");
+                    for (int j = 0; j < ds.Tables[i].Columns.Count; j++)
+                    {
+                        Console.WriteLine("Column name : " + ds.Tables[i].Columns[j].ColumnName);
+                        Console.WriteLine("Column type : " + ds.Tables[i].Columns[j].DataType.Name.ToString());
+                        Console.WriteLine("--------------------------------------");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        #endregion
+
+        #region AddSqlServerParam
+        void AddSqlServerParam(string key, string scriptText, Hashtable elements, IElement element, SqlCommand command, IGadgeteerBoard ParentBoard)
+        {
+            if (!_scripts.ContainsKey(key))
+            {
+                try
+                {
+                    _scripts.Add(key, ZenCsScriptCore.Initialize(ZenCsScriptCore.GetFunction("return " + scriptText + ";"), elements, element, Path.Combine("tmp", "SqlServer", element.ID + "_" + key + ".zen"), ParentBoard, element.GetElementProperty("PRINT_CODE") == "1"));
+                }
+                catch (Exception ex)
+                {
+                    string error = string.Empty;
+                    if (ex.InnerException != null && !string.IsNullOrEmpty(ex.InnerException.Message))
+                        error = ex.InnerException.Message;
+                    else
+                        error = ex.Message;
+
+                    throw new Exception("Error in parsing : " + scriptText + Environment.NewLine + error);
+                }
+            }
+            command.Parameters.AddWithValue(key, _scripts[key].ZenCsScript.RunCustomCode(_scripts[key].ScriptDoc.DocumentNode.Descendants("code").FirstOrDefault().Attributes["id"].Value));
+        }
+        #endregion
+
+        #region GetCacheFileName
+        string GetCacheFileName(IElement element)
+        {
+            return Path.Combine("tmp", "SqlServer", element.ID + ".zen");
+        }
+        #endregion
+
+        #region GetSqlStatement
+        string GetSqlStatement(string text, string file, Hashtable elements, IElement element, bool debug, IGadgeteerBoard ParentBoard)
+        {
+            lock (_syncCsScript)
+            {
+                if (_scriptData == null)
+                    _scriptData = ZenCsScriptCore.Initialize(text, elements, element, file, ParentBoard, debug);
+            }
+            return ZenCsScriptCore.GetCompiledText(text, _scriptData, elements, element, ParentBoard, file, debug);
+        }
+        #endregion
+    }
+
+
+
+    #endregion
+}
---
+++ Elements/NetFramework/ZenSqlServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ZenSqlServer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZenSqlServer")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("85553657-8c77-4a90-abcf-5f1cfa08eb7e")]
