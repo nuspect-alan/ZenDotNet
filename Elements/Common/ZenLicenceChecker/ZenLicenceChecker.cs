181264360560a925c8edc759a722eedbbc5b22fd
Tomaž Vinko <tomaz@zenodys.com>
2018-06-08 10:52:51 +0200

    ZenLicenceChecker: initial commit

---
+++ Elements/Common/ZenLicenceChecker/ZenLicenceChecker.cs
@@ -0,0 +1,266 @@
+﻿/*************************************************************************
+ * Copyright (c) 2015, 2018 Zenodys BV
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *    Tomaž Vinko
+ *   
+ **************************************************************************/
+
+using Nethereum.Signer;
+using Nethereum.Web3;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using ZenCommon;
+
+namespace ZenLicenceChecker
+{
+#if NETCOREAPP2_0
+    public class ZenLicenceChecker
+#else
+    public class ZenLicenceChecker : IZenEvent, IZenElementInit
+#endif
+    {
+        #region Constants
+        const string ABI = @"[{""constant"":true,""inputs"":[{""name"":""licenceId"",""type"":""bytes32""},{""name"":""customer"",""type"":""address""}],""name"":""checkLicence"",""outputs"":[{""name"":""licValid"",""type"":""bool""}],""payable"":false,""stateMutability"":""view"",""type"":""function""},{""constant"":true,""inputs"":[],""name"":""getPublicKey"",""outputs"":[{""name"":""publicKey"",""type"":""string""}],""payable"":false,""stateMutability"":""view"",""type"":""function""},{""constant"":false,""inputs"":[{""name"":""licenceId"",""type"":""bytes32""}],""name"":""confirmTransaction"",""outputs"":[{""name"":""success"",""type"":""bool""}],""payable"":false,""stateMutability"":""nonpayable"",""type"":""function""},{""constant"":true,""inputs"":[{""name"":"""",""type"":""bytes32""}],""name"":""_licences"",""outputs"":[{""name"":""customer"",""type"":""address""},{""name"":""price"",""type"":""uint256""},{""name"":""quantity"",""type"":""uint256""}],""payable"":false,""stateMutability"":""view"",""type"":""function""},{""constant"":false,""inputs"":[{""name"":""licenceId"",""type"":""bytes32""},{""name"":""customer"",""type"":""address""},{""name"":""price"",""type"":""uint256""},{""name"":""quantity"",""type"":""uint256""}],""name"":""addLicence"",""outputs"":[{""name"":""success"",""type"":""bool""}],""payable"":false,""stateMutability"":""nonpayable"",""type"":""function""},{""inputs"":[{""name"":""publicKey"",""type"":""string""}],""payable"":false,""stateMutability"":""nonpayable"",""type"":""constructor""}]";
+
+        #region LICENCE_CHECK_FUNCTION
+        const string LICENCE_CHECK_FUNCTION = "checkLicence";
+        #endregion
+
+        #region LENGTH_INFOS_SIZE
+        const int LENGTH_INFOS_SIZE = 4;
+        #endregion
+
+        #region FRAME_CNT
+        const int FRAME_CNT = 2;
+        #endregion
+        #endregion
+
+        #region Fields
+        #region _element
+        IElement _element;
+        #endregion
+
+        #region _serverPort
+        int _serverPort;
+        #endregion
+
+        #region _contractAddress
+        string _contractAddress;
+        #endregion
+
+        #region _ethProviderUrl
+        string _ethProviderUrl;
+        #endregion
+
+        #region _ownerAddress
+        string _ownerAddress;
+        #endregion
+
+        #region _ownerPassword
+        string _ownerPassword;
+        #endregion
+
+        #region _unlockDuration
+        int _unlockDuration;
+        #endregion
+
+        #region _web3
+        Web3 _web3;
+        #endregion
+
+        #endregion
+
+#if NETCOREAPP2_0
+        #region _implementations
+        static Dictionary<string, ZenLicenceChecker> _implementations = new Dictionary<string, ZenLicenceChecker>();
+        #endregion
+
+        unsafe public static void InitManagedElements(string currentElementId, void** elements, int elementsCount, int isManaged, string projectRoot, string projectId, ZenNativeHelpers.GetElementProperty getElementPropertyCallback, ZenNativeHelpers.GetElementResultInfo getElementResultInfoCallback, ZenNativeHelpers.GetElementResult getElementResultCallback, ZenNativeHelpers.ExecuteElement executeElementCallback, ZenNativeHelpers.SetElementProperty setElementProperty, ZenNativeHelpers.AddEventToBuffer addEventToBuffer)
+        {
+            if (!_implementations.ContainsKey(currentElementId))
+                _implementations.Add(currentElementId, new ZenLicenceChecker());
+
+            ZenNativeHelpers.InitManagedElements(currentElementId, elements, elementsCount, isManaged, projectRoot, projectId, getElementPropertyCallback, getElementResultInfoCallback, getElementResultCallback, executeElementCallback, setElementProperty, addEventToBuffer);
+            _implementations[currentElementId].InitVars(ZenNativeHelpers.Elements[currentElementId] as IElement);
+        }
+#else
+        #region IZenElementInit implementations
+        #region OnElementInit
+        public void OnElementInit(Hashtable elements, IElement element)
+        {
+            InitVars(element);
+        }
+        #endregion
+        #endregion
+
+        #region IZenEvent Implementations
+        #region Properties
+        #region ID
+        public string ID { get; set; }
+        #endregion
+
+        #region ParentBoard
+        public IGadgeteerBoard ParentBoard { get; set; }
+        #endregion
+        #endregion
+
+        #region Events
+        #region ModuleEvent
+        public event ModuleEventHandler ModuleEvent;
+        #endregion
+        #endregion
+
+        #region Functions
+        #region CheckInterruptCondition
+        public bool CheckInterruptCondition(ModuleEventData eventData, IElement element, Hashtable plugins)
+        {
+            element.LastResultBoxed = eventData.Tag;
+            return true;
+        }
+        #endregion
+        #endregion
+        #endregion
+#endif
+
+        #region Private functions
+        void InitVars(IElement element)
+        {
+            _serverPort = Convert.ToInt32(element.GetElementProperty("SERVER_PORT"));
+            _ethProviderUrl = element.GetElementProperty("ETH_PROVIDER_URL");
+            _ownerAddress = element.GetElementProperty("OWNER_ADDRESS");
+            _ownerPassword = element.GetElementProperty("OWNER_PASSWORD");
+            _unlockDuration = Convert.ToInt32(element.GetElementProperty("UNLOCK_DURATION"));
+            _contractAddress = element.GetElementProperty("CONTRACT_ADDRESS");
+            _web3 = new Web3(_ethProviderUrl);
+            _element = element;
+            new Thread(WaitRequest).Start();
+        }
+        #region Decrypt
+        string Decrypt(byte[] encryptedData)
+        {
+            string decrpytedText = string.Empty;
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.FromXmlString(File.ReadAllText("keystore/private.xml"));
+                var decryptedBytes = rsa.Decrypt(encryptedData, true);
+                decrpytedText = Encoding.UTF8.GetString(decryptedBytes);
+            }
+            return decrpytedText;
+        }
+        #endregion
+
+        #region GetRequestBuffer
+        byte[] GetRequestBuffer(Stream stream)
+        {
+            byte[] request = null;
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                request = ms.ToArray();
+            }
+            return request;
+        }
+        #endregion
+
+        #region WaitRequest
+        async void WaitRequest()
+        {
+            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), _serverPort);
+            server.Start();
+            Console.WriteLine("Start listening for asset requests on port {0}.", _serverPort);
+
+            while (true)
+            {
+                byte[] rawRequest = null;
+                string clientIP = string.Empty;
+
+                using (TcpClient client = server.AcceptTcpClient())
+                {
+                    NetworkStream stream = client.GetStream();
+                    rawRequest = GetRequestBuffer(stream);
+                    clientIP = ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString();
+                    client.Close();
+                }
+
+                int licenceIdLength = BitConverter.ToInt32(rawRequest, 0 * LENGTH_INFOS_SIZE);
+                int signatureLength = BitConverter.ToInt32(rawRequest, 1 * LENGTH_INFOS_SIZE);
+                int publicKeyLength = BitConverter.ToInt32(rawRequest, 2 * LENGTH_INFOS_SIZE);
+                int callbackUrlLength = BitConverter.ToInt32(rawRequest, 3 * LENGTH_INFOS_SIZE);
+
+                byte[] licenceIdRaw = new byte[licenceIdLength];
+                byte[] signatureRaw = new byte[signatureLength];
+                byte[] publicKeyRaw = new byte[publicKeyLength];
+                byte[] callbackPortRaw = new byte[callbackUrlLength];
+
+                Array.Copy(rawRequest, 4 * LENGTH_INFOS_SIZE, licenceIdRaw, 0, licenceIdLength);
+
+                Array.Copy(rawRequest, 4 * LENGTH_INFOS_SIZE + licenceIdLength, signatureRaw,
+                           0, signatureLength);
+
+                Array.Copy(rawRequest, 4 * LENGTH_INFOS_SIZE + licenceIdLength + signatureLength,
+                    publicKeyRaw, 0, publicKeyLength);
+
+                Array.Copy(rawRequest, 4 * LENGTH_INFOS_SIZE + licenceIdLength + signatureLength + publicKeyLength,
+                    callbackPortRaw, 0, callbackUrlLength);
+
+                string licenceId = Encoding.Default.GetString(licenceIdRaw);
+                // Decrypt signed licenceId with asset owner private key
+                string signature = Decrypt(signatureRaw);
+                string customerPubKey = Encoding.Default.GetString(publicKeyRaw);
+                string callbackPort = Encoding.Default.GetString(callbackPortRaw);
+
+                // Get address from validation process
+                var address = new MessageSigner().HashAndEcRecover(licenceId, signature);
+
+                var unlockAccountResult = await _web3.Personal.UnlockAccount.SendRequestAsync(_ownerAddress,
+                                                _ownerPassword, _unlockDuration);
+
+                // Validate smart contract if provided address has access to asset which belongs to current licenceId
+                bool isLicenceValid = await _web3.Eth.GetContract(ABI, _contractAddress)
+                                            .GetFunction(LICENCE_CHECK_FUNCTION)
+                                            .CallAsync<bool>(licenceId, address);
+
+                Console.WriteLine("Licence: {0}; valid: {1}, ip: {2}", licenceId, isLicenceValid.ToString(), clientIP);
+                // If verification succed, trigger element complete event and save licenceId, 
+                // customer public key and callback url
+                // Parameters will be needed in other elements
+#if NETCOREAPP2_0
+                ZenNativeHelpers.AddEventToBufferCallback(_element.Ptr, string.Concat(licenceId, ";",
+                                                                            customerPubKey, ";", clientIP, ";", callbackPort));
+#else
+                if (isLicenceValid && ModuleEvent != null)
+                    ModuleEvent(this, new ModuleEventData(_element.ID, string.Empty, string.Concat(licenceId, ";",
+                                                                            customerPubKey, ";", clientIP, ";", callbackPort)));
+#endif
+            }
+        }
+        #endregion
+        #endregion
+    }
+}
