263d47352cfd2d0bd9c67e924d46967870a1d980
Tomaž Vinko <tomaz@zenodys.com>
2018-10-08 19:10:58 +0200

    publish.bat initial commit

---
+++ Elements/NetCore/publish.bat
@@ -0,0 +1,33 @@
+@echo off
+	
+    setlocal enableextensions disabledelayedexpansion
+
+    set "newDir=..\..\"
+
+    call :resolve "%newDir%" resolvedDir
+	
+	set SolutionDir=%resolvedDir%
+    
+	if exist "%SolutionDir%\Implementations" rmdir %SolutionDir%\Implementations /S /Q
+	
+	dotnet build --configuration Release %cd%\ZenAssetReceiver\ZenAssetReceiver.csproj
+	dotnet build --configuration Release %cd%\ZenAssetTransmitter\ZenAssetTransmitter.csproj
+	dotnet build --configuration Release %cd%\ZenCsScript\ZenCsScript.csproj
+	dotnet build --configuration Release %cd%\ZenDebug\ZenDebug.csproj
+	dotnet build --configuration Release %cd%\ZenElementActionCaller\ZenElementActionCaller.csproj
+	dotnet build --configuration Release %cd%\ZenElementsExecuter\ZenElementsExecuter.csproj
+	dotnet build --configuration Release %cd%\ZenExcelReader\ZenExcelReader.csproj
+	dotnet build --configuration Release %cd%\ZenGwSystemInfo\ZenGwSystemInfo.csproj
+	dotnet build --configuration Release %cd%\ZenHttpRequest\ZenHttpRequest.csproj
+	dotnet build --configuration Release %cd%\ZenLicenceChecker\ZenLicenceChecker.csproj
+	dotnet build --configuration Release %cd%\ZenMail\ZenMail.csproj
+	dotnet build --configuration Release %cd%\ZenMySql\ZenMySql.csproj
+	dotnet build --configuration Release %cd%\ZenSqlServer\ZenSqlServer.csproj
+	dotnet build --configuration Release %cd%\ZenWebServer\ZenWebServer.csproj
+	
+    endlocal
+    goto :EOF
+
+:resolve file/folder returnVarName
+    set "%~2=%~f1"
+    goto :EOF
