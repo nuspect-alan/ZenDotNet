6c0df4f416e28998315e8fc34ac7e2d84372f090
tvinko <tomaz@zenodys.com>
2019-10-06 11:32:49 +0200

    Copyright notice

--- Elements/NetCore/ZenGwSystemInfo/CpuUsage.cs
+++ Elements/NetCore/ZenGwSystemInfo/CpuUsage.cs
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Copyright (c) 2015, 2018 Zenodys BV
+ * Copyright (c) 2015, 2019 Zenodys BV
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
